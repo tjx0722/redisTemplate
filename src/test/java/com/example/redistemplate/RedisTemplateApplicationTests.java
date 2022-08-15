6c57e5bc0fd41d2bfe51a9df3474f80150fdd4a4
tianjiaxin <tianjiaxin@inspur.com>
2022-08-15 17:15:13 +0800

    Feated；提交代码

---
+++ src/test/java/com/example/redistemplate/RedisTemplateApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.redistemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
